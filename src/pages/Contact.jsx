79f0d9d62075339bcb8b39f411e828a0180d30a2
ajibroziq21@gmail.com <ajibroziq21@gmail.com>
2024-04-07 23:20:16 +0700

    feat: 07/04/2024 - create function fox animation & hook Alert

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+
+import Loader from "../components/Loader";
+import Fox from "../models/Fox";
+import useAlert from "../hooks/useAlert";
+import Alert from "../components/Alert";
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const { alert, showAlert, hideAlert } = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleFocus = () => {};
-  const handleBlur = () => {};
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation("hit");
 
     emailjs
       .send(
@@ -22,8 +30,8 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Ajib Roziq",
           from_email: form.email,
+          to_name: "Ajib Roziq",
           to_email: "arozproject@gmail.com",
           message: form.message,
         },
@@ -31,14 +39,36 @@ const Contact = () => {
       )
       .then(() => {
         setIsLoading(false);
+        showAlert({
+          show: true,
+          text: "Message sent successfully",
+          type: "success",
+        });
+
+        setTimeout(() => {
+          hideAlert();
+          setCurrentAnimation("idle");
+          setForm({ name: "", email: "", message: "" });
+        }, [3000]);
       })
       .catch((error) => {
+        setCurrentAnimation("idle");
         setIsLoading(false), console.log(error);
+        showAlert({
+          show: true,
+          text: "I didnt receive your message",
+          type: "danger",
+        });
       });
   };
 
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
+
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get In Touch</h1>
 
@@ -79,7 +109,6 @@ const Contact = () => {
               name="message"
               rows={5}
               className="textarea"
-              required
               placeholder="Let me know your message!"
               value={form.message}
               onChange={handleChange}
@@ -96,6 +125,27 @@ const Contact = () => {
             {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}>
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
